5a4a673f8486b1b672ff49bfe7857cb6628608d8
ansrlgur12 <ansrlgur12@naver.com>
2023-08-13 14:58:20 +0900

    html 복습, 초기 세팅 플러그인 복습

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var dev = '문기혁';
+// 타입 지정
+var fruit = 'apple';
+var animals = ['dog', 'cat', 'bird'];
+var sports = [{ name: 'soccer' }, { name: 'baseball' }];
+var 이름 = 'Moon';
+var a = 123;
+// 함수 파라미터에 타입 지정
+function func(x) {
+    return x * 2;
+}
+func(123);
+var player = [7, 'son'];
--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-let dev :string = '문기혁'
+// 타입 지정
+let fruit :string = 'apple';
+let animals :string[] = ['dog', 'cat', 'bird'];
+let sports :{name : string}[] = [{name : 'soccer'}, {name : 'baseball'}];
+
+let 이름 :string | number = 'Moon'
+
+// 변수로 타입 지정
+type myType = string | number;
+let a :myType = 123;
+
+// 함수 파라미터에 타입 지정
+function func(x: number){
+    return x * 2
+}
+
+func(123);
+
+// array에 쓸 수 있는 tuple 타입
+type BackNumber = [number, string];
+
+let player:BackNumber = [7, 'son'];
